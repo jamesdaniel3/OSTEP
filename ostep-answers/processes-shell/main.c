a96321be5af7771de66caf5fc9b3be4f9ab382f5
James Daniel <jamesmd333@gmail.com>
2026-07-18 16:51:55 -0400

    added the ablity to read from a file

--- ostep-answers/processes-shell/main.c
+++ ostep-answers/processes-shell/main.c
@@ -9,14 +9,18 @@ size_t num_paths = 1;
 char* paths[1] = { "/bin/" };
 char error_message[30] = "An error has occurred\n";
 
+void exit_shell(){
+    fwrite(error_message, strlen(error_message), 1, stderr); 
+    exit(1);
+}
+
 char* find_path(char * program){
     char * result;
     for(size_t i = 0; i < num_paths; i++){
         size_t path_size = strlen(paths[i]) + strlen(program) + 1; 
         result = realloc(NULL, path_size);
         if (result == NULL){
-            fwrite(error_message, strlen(error_message), 1, stderr); 
-            exit(1);
+            exit_shell();
         }
 
         memcpy(result, paths[i], strlen(paths[i]));
@@ -43,8 +47,7 @@ arg_arr get_args(char* line){
     size_t cap = 5;
     char** args = malloc(sizeof(char *) * cap);
     if (args == NULL) {
-        fwrite(error_message, strlen(error_message), 1, stderr); 
-        exit(1);
+        exit_shell();
     }
 
     char** arg_pointer;
@@ -55,8 +58,7 @@ arg_arr get_args(char* line){
                 cap *= 2;
                 args = reallocf(args, sizeof(char *) * cap);
                 if (args == NULL){
-                    fwrite(error_message, strlen(error_message), 1, stderr); 
-                    exit(1);
+                    exit_shell();
                 }
             }
         }
@@ -76,11 +78,18 @@ arg_arr get_args(char* line){
 
 int main(int argc, char* argv[]){
     if(argc > 2) {
-        fwrite(error_message, strlen(error_message), 1, stderr); 
-        exit(1);
+        exit_shell();
     }
 
     bool is_interactive_mode = argc == 1;
+    FILE * fp = NULL;
+    if (!is_interactive_mode) {
+        fp = fopen(argv[1], "r");
+
+        if (fp == NULL) {
+            exit_shell();
+        }
+    }
 
     char* line = NULL;
     size_t line_cap = 0;
@@ -89,13 +98,13 @@ int main(int argc, char* argv[]){
     if (is_interactive_mode) {
          printf("wish> ");
     }
-    while ((linelen = getline(&line, &line_cap, stdin)) > 0) {
+
+    while ((linelen = getline(&line, &line_cap, is_interactive_mode ? stdin : fp)) > 0) {
         arg_arr line_args = get_args(line);            // don't forget to clear line_args!!
         char* path = find_path(line_args.args[0]);     // don't forget to clear path!!
 
         if (path == NULL) {
-            fwrite(error_message, strlen(error_message), 1, stderr); 
-            exit(1);
+            exit_shell();
         }
         line_args.args[0] = path;  
 
@@ -104,8 +113,7 @@ int main(int argc, char* argv[]){
         if (pid == 0) {
             int result = execv(line_args.args[0], line_args.args);
             if (result < 0) {
-                fwrite(error_message, strlen(error_message), 1, stderr); 
-                exit(1);
+                exit_shell();
             }
         }
 
@@ -113,12 +121,13 @@ int main(int argc, char* argv[]){
             int stat;
             int cpid = waitpid(pid, &stat, WUNTRACED);
             if (cpid < 0) {
-                fwrite(error_message, strlen(error_message), 1, stderr); 
-                exit(1);
+                exit_shell();
             }
         }
 
-        printf("wish> ");
+        if (is_interactive_mode) {
+            printf("wish> ");
+        }
     }
 
     return 0;
